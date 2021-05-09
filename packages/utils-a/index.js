6ee406045f2ef69b67d58e4ed43dc55ca6901ac8
Artium Wasse <artium.wasse@vimcar.com>
2021-05-09 22:01:02 +0200

    feat: Add console log

--- packages/utils-a/index.js
+++ packages/utils-a/index.js
@@ -2,4 +2,5 @@ var randomSentence = require("random-sentence");
 
 module.exports = () => {
   console.log(`utils-a: ${randomSentence({ words: 5 })}`);
+  console.log(`utils-a: ${randomSentence({ words: 5 })}`);
 };
